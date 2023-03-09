5b685257ba96dbb1bdb6eb9e0efec0560ccd71a1
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-03-10 00:40:34 +0500

    FEAT: Setting up a basic struture

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <nav>
+      <div className="flex flex-wrap justify-between item-center">
+        <div className="rounded-full bg-green-600">
+          <h1>S</h1>
+        </div>
+      </div>
+    </nav>
+  );
+}
