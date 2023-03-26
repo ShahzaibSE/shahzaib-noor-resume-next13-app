82c5fea37ade6cd5f4c317f0175141af5af25640
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-03-27 00:07:55 +0500

    FEAT: Adding some more information about personal skills.

--- components/home/HomeContainer.tsx
+++ components/home/HomeContainer.tsx
@@ -49,7 +49,29 @@ export default function HomeContainer() {
       </div>
       <div className="flex flex-row p-5 md:p-5 lg:p-10">
         <div className="flex">
-          <h1 className="text-white font-semibold text-2xl decoration-blue-500 underline">What I Do</h1>
+          <h1 className="text-2xl font-semibold text-white underline decoration-blue-500">
+            What I Do
+          </h1>
+        </div>
+      </div>
+      <div className="flex flex-row px-5 md:px-5 lg:px-10">
+        <div className="flex flex-wrap items-center justify-around">
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6 text-white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
