a41a168eaceb21f824b2aa6a0cea20df97655ea0
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-04 03:07:31 +0500

    FEAT: Adding a responsive footer to home page.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,34 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 import {
   FaGithub,
   FaTwitter,
   FaYoutube,
   FaFacebook,
 } from "react-icons/fa";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="flex flex-wrap p-10">
-      <div className="flex justify-start items-center">
-        <div className="flex justify-start items-center">
-          <div className="flex px-5">
-            <FaFacebook className="text-white" />
-          </div>
-          <div className="flex px-5">
-            <FaTwitter className="text-white" />
-          </div>
-          <div className="flex px-5">
-            <FaGithub className="text-white" />
-          </div>
+    <div className="flex flex-wrap p-10 justify-between">
+      <div className="flex items-center justify-around sm:justify-center">
+        <div className="flex px-5">
+          <FaFacebook className="text-white" />
+        </div>
+        <div className="flex px-5">
+          <FaTwitter className="text-white" />
+        </div>
+        <div className="flex px-5">
+          <FaGithub className="text-white" />
         </div>
       </div>
-      <div className="flex justify-end items-center">
-        <div className="flex justify-end">
-          <h1 className="text-white text-right text-sm">© 2023 All rights reserved. Shahzaib Noor.</h1>
-        </div>
+      <div className="flex items-center justify-end">
+        <h1 className="text-right text-sm text-white">
+          © 2023 All rights reserved. Shahzaib
+          Noor.
+        </h1>
       </div>
     </div>
-  )
+  );
 }
