bee902996720a71822386a29d8367aad8168f47c
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-04 23:52:04 +0500

    Adding opacity for mobile screens.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    CONTENTFUL_SPACE_ID: "zqwab88shp19",
+    CONTENTFUL_ACCESS_TOKEN:"I0MIN90DLAJRfk_48_WZ6kXOUzQ-hWQpJM-sj9K0RiE",
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN:"MoE1rjhPMy6fH5P010yhLEVkNNQEVsb8RMkD4rV1ENI",
+  },
 }
 
 module.exports = nextConfig
