f84dfc2b75d3bcd487f9ea0d061be8e1c24e1f6f
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-08 00:01:31 +0500

    FEAT: Adding section to resume page and making it responsive

--- components/resume/ResumeContainer.tsx
+++ components/resume/ResumeContainer.tsx
@@ -13,7 +13,14 @@ import { motion } from "framer-motion";
 export default function ResumeContainer() {
   return (
     <div className="container">
-      
+      <div className="flex flex-wrap justify-between border-t-gray-300 border-t-2 border-b-2 bg-neutral-800 border-b-gray-300 h-1/4 p-10 my-10">
+        <div className="flex">
+          <h1 className="text-white font-bold text-xl md:text-2xl lg:text-3xl">Resume</h1>
+        </div>
+        <div className="flex">
+          <h3 className="text-white font-semibold text-sm">6 Years of Experience</h3>
+        </div>
+      </div>
     </div>
   );
 }
