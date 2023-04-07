f51ccb76d5775dca2db312e0d83f76ee0677cdeb
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-08 02:35:22 +0500

    FEAT: Making font responsive on time line element.

--- components/resume/ResumeContainer.tsx
+++ components/resume/ResumeContainer.tsx
@@ -25,9 +25,9 @@ export default function ResumeContainer() {
           </h3>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-2 gap-2 grid-flow-row-dense">
         <div>
-          <div className="flex py-5">
+          <div className="flex p-5 md:py-5">
             <h2 className="text-lg font-bold text-white">
               Education
             </h2>
@@ -38,45 +38,27 @@ export default function ResumeContainer() {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 February 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 Application UI code in Tailwind
                 CSS
               </h3>
-              <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
                 Get access to over 20+ pages
                 including a dashboard layout,
                 charts, kanban board, calendar,
                 and pre-order E-commerce &
                 Marketing pages.
               </p>
-              <a
-                href="#"
-                className="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-              >
-                Learn more{" "}
-                <svg
-                  className="ml-2 h-3 w-3"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
             </li>
             <li className="mb-10 ml-4">
               <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 March 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 Marketing UI design in Figma
               </h3>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
                 All of the pages and components
                 are first designed in Figma and we
                 keep a parity between the two
@@ -89,10 +71,10 @@ export default function ResumeContainer() {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 April 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 E-Commerce UI code in Tailwind CSS
               </h3>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
                 Get started with dozens of web
                 components and interactive
                 elements built on top of Tailwind
@@ -102,7 +84,7 @@ export default function ResumeContainer() {
           </ol>
         </div>
         <div>
-          <div className="flex py-5">
+          <div className="flex p-5 md:py-5">
             <h2 className="text-lg font-bold text-white">
               Experience
             </h2>
@@ -113,45 +95,27 @@ export default function ResumeContainer() {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 February 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 Application UI code in Tailwind
                 CSS
               </h3>
-              <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
                 Get access to over 20+ pages
                 including a dashboard layout,
                 charts, kanban board, calendar,
                 and pre-order E-commerce &
                 Marketing pages.
               </p>
-              <a
-                href="#"
-                className="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-              >
-                Learn more{" "}
-                <svg
-                  className="ml-2 h-3 w-3"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
             </li>
             <li className="mb-10 ml-4">
               <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 March 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 Marketing UI design in Figma
               </h3>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
                 All of the pages and components
                 are first designed in Figma and we
                 keep a parity between the two
@@ -164,10 +128,10 @@ export default function ResumeContainer() {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 April 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                 E-Commerce UI code in Tailwind CSS
               </h3>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
                 Get started with dozens of web
                 components and interactive
                 elements built on top of Tailwind
---
+++ components/skills/Skills.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div>Skills</div>
+  )
+}
