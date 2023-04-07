c0e9470e9268851f0c6ff8a1da5e8827bb553098
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-08 02:45:10 +0500

    FEAT: Added wrapping feature.

--- components/resume/ResumeContainer.tsx
+++ components/resume/ResumeContainer.tsx
@@ -25,63 +25,66 @@ export default function ResumeContainer() {
           </h3>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-2 grid-flow-row-dense">
+      <div className="grid grid-flow-row-dense grid-cols-2 gap-2 flex-wrap">
         <div>
           <div className="flex p-5 md:py-5">
             <h2 className="text-lg font-bold text-white">
               Education
             </h2>
           </div>
-          <ol className="relative border-l border-gray-200 dark:border-gray-700">
-            <li className="mb-10 ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                February 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                Application UI code in Tailwind
-                CSS
-              </h3>
-              <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
-                Get access to over 20+ pages
-                including a dashboard layout,
-                charts, kanban board, calendar,
-                and pre-order E-commerce &
-                Marketing pages.
-              </p>
-            </li>
-            <li className="mb-10 ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                March 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                Marketing UI design in Figma
-              </h3>
-              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                All of the pages and components
-                are first designed in Figma and we
-                keep a parity between the two
-                versions even as we update the
-                project.
-              </p>
-            </li>
-            <li className="ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                April 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                E-Commerce UI code in Tailwind CSS
-              </h3>
-              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                Get started with dozens of web
-                components and interactive
-                elements built on top of Tailwind
-                CSS.
-              </p>
-            </li>
-          </ol>
+          <div className="flex flex-wrap">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+              <li className="mb-10 ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  February 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  Application UI code in Tailwind
+                  CSS
+                </h3>
+                <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Get access to over 20+ pages
+                  including a dashboard layout,
+                  charts, kanban board, calendar,
+                  and pre-order E-commerce &
+                  Marketing pages.
+                </p>
+              </li>
+              <li className="mb-10 ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  March 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  Marketing UI design in Figma
+                </h3>
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  All of the pages and components
+                  are first designed in Figma and
+                  we keep a parity between the two
+                  versions even as we update the
+                  project.
+                </p>
+              </li>
+              <li className="ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  April 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  E-Commerce UI code in Tailwind
+                  CSS
+                </h3>
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Get started with dozens of web
+                  components and interactive
+                  elements built on top of
+                  Tailwind CSS.
+                </p>
+              </li>
+            </ol>
+          </div>
         </div>
         <div>
           <div className="flex p-5 md:py-5">
@@ -89,56 +92,59 @@ export default function ResumeContainer() {
               Experience
             </h2>
           </div>
-          <ol className="relative border-l border-gray-200 dark:border-gray-700">
-            <li className="mb-10 ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                February 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                Application UI code in Tailwind
-                CSS
-              </h3>
-              <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
-                Get access to over 20+ pages
-                including a dashboard layout,
-                charts, kanban board, calendar,
-                and pre-order E-commerce &
-                Marketing pages.
-              </p>
-            </li>
-            <li className="mb-10 ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                March 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                Marketing UI design in Figma
-              </h3>
-              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                All of the pages and components
-                are first designed in Figma and we
-                keep a parity between the two
-                versions even as we update the
-                project.
-              </p>
-            </li>
-            <li className="ml-4">
-              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                April 2022
-              </time>
-              <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
-                E-Commerce UI code in Tailwind CSS
-              </h3>
-              <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                Get started with dozens of web
-                components and interactive
-                elements built on top of Tailwind
-                CSS.
-              </p>
-            </li>
-          </ol>
+          <div className="flex flex-wrap">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+              <li className="mb-10 ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  February 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  Application UI code in Tailwind
+                  CSS
+                </h3>
+                <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Get access to over 20+ pages
+                  including a dashboard layout,
+                  charts, kanban board, calendar,
+                  and pre-order E-commerce &
+                  Marketing pages.
+                </p>
+              </li>
+              <li className="mb-10 ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  March 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  Marketing UI design in Figma
+                </h3>
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  All of the pages and components
+                  are first designed in Figma and
+                  we keep a parity between the two
+                  versions even as we update the
+                  project.
+                </p>
+              </li>
+              <li className="ml-4">
+                <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  April 2022
+                </time>
+                <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
+                  E-Commerce UI code in Tailwind
+                  CSS
+                </h3>
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Get started with dozens of web
+                  components and interactive
+                  elements built on top of
+                  Tailwind CSS.
+                </p>
+              </li>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
