b6845d67a5f3b75934028b3241075291f5a2c2cf
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-19 00:09:39 +0500

    REF: Refractored code of navigation bar.

--- components/Header.tsx
+++ components/Header.tsx
@@ -145,10 +145,7 @@ export default function Header() {
           className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
           aria-controls="navbar-default"
           aria-expanded="false"
-          onClick={() => {
-            console.log(navbar);
-            return setNavbar(!navbar);
-          }}
+          onClick={() => setNavbar(!navbar)}
         >
           <span className="sr-only">
             Open main menu
