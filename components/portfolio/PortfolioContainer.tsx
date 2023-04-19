4a39b0de448430fe2e8227136e25c9da2b23dbd8
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-19 23:33:08 +0500

    FEAT: Setting up a responsive reusable component for projects.

--- components/portfolio/PortfolioContainer.tsx
+++ components/portfolio/PortfolioContainer.tsx
@@ -1,12 +1,19 @@
 'use client';
 import React from 'react';
-// import {ProjectContainer} from ""
+import { Project, ProjectCard } from '../utitlities/utils';
 
 export default function PortfolioContainer() {
-  const projects = [];
+  const projects: Array<Project> = [
+    {
+      name: "Panaverse DAO with Chakra-UI",
+      imageUrl: "assets/panaverse-dao.png"
+    }
+  ];
   return (
-    <div className="container">
-
+    <div className="container flex flex-wrap justify-evenly items-center">
+      {
+        projects.map((project: Project, index: number) => <ProjectCard key={index} name={project.name} imageUrl={project.imageUrl} />)
+      }
     </div>
   )
 }
--- components/utitlities/utils.tsx
+++ components/utitlities/utils.tsx
@@ -1,5 +1,12 @@
 'use client';
 import Image from "next/image";
+import {
+  FaGithub,
+  FaTwitter,
+  FaYoutube,
+  FaFacebook,
+} from "react-icons/fa";
+import Link from "next/link";
 
 export const particle_config = {
   autoPlay: true,
@@ -1133,7 +1140,7 @@ export const variants = {
 
 export interface Project {
   name: string;
-  description: string;
+  description?: string;
   imageUrl: string;
 }
 
@@ -1153,27 +1160,19 @@ export function ProjectCard({name, description, imageUrl}:Project) {
             {name}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {description}
+        {
+         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description} : 
         </p>
+        }
         <a
           href="#"
           className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Read more
-          <svg
-            aria-hidden="true"
-            className="ml-2 -mr-1 h-4 w-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          Source Code
+          <Link href="https://github.com/ShahzaibSE" target="_blank">
+            <FaGithub className="text-white" />
+          </Link>
         </a>
       </div>
     </div>
