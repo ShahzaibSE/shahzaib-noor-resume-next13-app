5ba1aa9f0395243e617ad93e3e110c1451c08f9d
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-20 00:59:09 +0500

    REF: Adding projects.

--- components/portfolio/PortfolioContainer.tsx
+++ components/portfolio/PortfolioContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { Project, ProjectCard } from '../utitlities/utils';
+import { motion } from "framer-motion";
 
 export default function PortfolioContainer() {
   const projects: Array<Project> = [
@@ -8,12 +9,17 @@ export default function PortfolioContainer() {
       name: "Panaverse DAO with Chakra-UI",
       imageUrl: "/assets/panaverse-dao.png",
       sourceCodeUrl: "https://github.com/ShahzaibSE/panaverse-dao-syllabus-nextjs-website.git"
+    },
+    {
+      name: "Rive Rocket Animation",
+      imageUrl: "/assets/rive-rocket.png",
+      sourceCodeUrl: "https://github.com/ShahzaibSE/rocket-rive-animation-next-app.git"
     }
   ];
   return (
     <div className="container flex flex-wrap justify-evenly items-center p-10">
       {
-        projects.map((project: Project, index: number) => <ProjectCard key={index} name={project.name} imageUrl={project.imageUrl} sourceCodeUrl={project.sourceCodeUrl} />)
+        projects.map((project: Project, index: number) =>  <motion.div key={index} whileHover={{ scale: 1.2 }}><ProjectCard key={index} name={project.name} imageUrl={project.imageUrl} sourceCodeUrl={project.sourceCodeUrl} /></motion.div>)
       }
     </div>
   )
